b3d9f8878d351086c07ed3e50bd3a668f5ebc34b
Saket Ramsinghani <saket030801@gmail.com>
2024-11-07 23:10:48 +0530

    user can now ask question from terminal as a process argument

--- index.js
+++ index.js
@@ -1,25 +1,15 @@
-import OpenAI from "openai";
 
-const openai = new OpenAI({apiKey:""});
-//const OPENAI_API_URL = "https://api.openai.com/v1/chat/completions"
+import {runLLM} from './src/llm.js'
 
-async function completion() {
-    
-    const response = await openai.chat.completions.create({
-        model: "gpt-4o-mini",
-        messages: [
-            {
-                "role": "system",
-                "content": "You are a helpful assistant."
-            },
-            {
-                "role": "user",
-                "content": "Write a haiku that explains the concept of recursion."
-            }
-        ]
-    })
+const userMessage = process.argv[2];
 
-    console.log(response.choices[0].message.content)
+if(!userMessage){
+    console.error("Please provide a message")
+    process.exit(1);
 }
 
-completion()
+const response = await runLLM({
+    messages: [{role: 'user', content: userMessage}]
+})
+
+console.log(response);
---
+++ src/llm.js
@@ -0,0 +1,16 @@
+import { openai } from "./ai.js";
+
+export const runLLM = async ({
+    model = 'gpt-4o-mini',
+    messages,
+    temperature= 0.1
+}) => {
+    const response = await openai.chat.completions.create({
+        model,
+        messages,
+        temperature
+    });
+
+    return response.choices[0].message;
+}
+
